35a40bf009230d4f9819acc209e39d0bef67453d
Babu Moger <babu.moger@oracle.com>
2023-06-22 21:17:27 +0530

    linux: Remove the check #ifndef __LINUX_SPINLOCK_TYPES_H
    Fix:
    ../include/linux/pm_wakeup.h:26:3: error: "please don't include this file directly"
      ^
    
    Signed-off-by: Cykeek <Cykeek@proton.me>
    Suggested-by: David Miller <davem@davemloft.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/pm_wakeup.h
+++ include/linux/pm_wakeup.h
@@ -22,10 +22,6 @@
 #ifndef _LINUX_PM_WAKEUP_H
 #define _LINUX_PM_WAKEUP_H
 
-#ifndef _DEVICE_H_
-# error "please don't include this file directly"
-#endif
-
 #include <linux/types.h>
 
 struct wake_irq;
