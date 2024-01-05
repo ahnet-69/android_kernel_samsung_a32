22a57937fc109e4e168095989faad96fa835522d
ahnet-69 <syedmusicrealise@gmail.com>
2024-01-05 16:12:05 +0000

    Use all threads for building

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,7 +9,7 @@ export ANDROID_MAJOR_VERSION=r
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a32_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc --all) a32_defconfig
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j$(nproc --all)
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
